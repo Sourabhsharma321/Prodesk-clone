ce0b800d1a5736d7b4afe206eaf5468db7e41aaf
Sourabh Sharma <152863038+Sourabhsharma321@users.noreply.github.com>
2026-02-16 12:10:51 +0530

    Delete poddesk/prodesk directory

--- poddesk/prodesk/src/main/java/org/sourabh/prodesk/repository/CareerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sourabh.prodesk.repository;
-
-import org.sourabh.prodesk.entity.Career;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CareerRepository extends JpaRepository<Career, Long> {
-}
--- poddesk/prodesk/src/main/java/org/sourabh/prodesk/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sourabh.prodesk.repository;
-
-import org.sourabh.prodesk.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
